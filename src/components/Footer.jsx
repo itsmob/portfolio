ccc79d643dcaa1482fbe4e1051bfed298929d0c6
gilberto <gilbertogalindez96@gmail.com>
2023-05-27 10:09:41 -0400

    ADD targets blanks at footer anchors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <footer className='footer__container'>
         <div className='footer__content'>
           <p className='footer__p'>This design is stolen</p>
-          <a className='footer__a' href='https://discord.gg/8qCEQPQqRq'>
+          <a className='footer__a' href='https://discord.gg/8qCEQPQqRq' target='_blank'>
             <span className='footer__a--discord-icon'></span>
           </a>
-          <a className='footer__a' href='https://github.com/mobvnzla'>
+          <a className='footer__a' href='https://github.com/mobvnzla' target='_blank'>
             <span className='footer__a--github-icon'></span>
           </a>
         </div>
