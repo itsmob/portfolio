fd724c3ab15a77f713b918f2349895ebb566f609
itsmob <mobster.dev.96@gmail.com>
2024-02-21 10:28:24 -0400

    style: :lipstick: Change profile picture

--- src/components/FramePicture.jsx
+++ src/components/FramePicture.jsx
@@ -4,7 +4,7 @@ export default function FramePicture() {
   return (
     <div className='img-container'>
       <div className='box'>
-        <img src='/images/profile-picture.jpg' alt='' />
+        <img src='/images/profile-picture.png' alt='' />
       </div>
     </div>
   );
